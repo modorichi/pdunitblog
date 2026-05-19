f741a4b186b6039cdb59f19e11c3dcd492e97386
kuniyasu-e <kuniyasu-e@mainichi.co.jp>
2026-05-19 17:35:41 +0900

    redeploy

--- server/api/blogs/index.get.ts
+++ server/api/blogs/index.get.ts
@@ -3,8 +3,19 @@ import { createClient } from 'microcms-js-sdk';
 export default defineEventHandler(async () => {
   const config = useRuntimeConfig();
 
-  console.log('serviceDomain:', config.public.microcmsServiceDomain);
-  console.log('apiKey exists:', !!config.microcmsApiKey);
+  if (!config.public.microcmsServiceDomain) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'MICROCMS_SERVICE_DOMAIN is missing',
+    });
+  }
+
+  if (!config.microcmsApiKey) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'MICROCMS_API_KEY is missing',
+    });
+  }
 
   const client = createClient({
     serviceDomain: config.public.microcmsServiceDomain,
